01859f0f937b47a78b83871eade6c63b63a34e34
Hayoung Gam <hahhyoung@gmail.com>
2021-07-07 02:45:20 +0900

    Add: 상세페이지 상세정보 area 탭메뉴 UI 구현

---
+++ src/pages/product/Components/ContentsTab.jsx
@@ -0,0 +1,31 @@
+import { Component } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import './Styles/ContentsTab.scss';
+
+class ContentsTab extends Component {
+  render() {
+    const { area } = this.props;
+    const { pathname } = this.props.location;
+    return (
+      <ul className="contentsTab">
+        <li className={area === 'detail' ? 'tabItem active' : 'tabItem'}>
+          <Link
+            className="tabLink"
+            to={{ pathname: pathname, hash: `#${area}` }}
+          >
+            상세정보
+          </Link>
+        </li>
+        <li className={area === 'review' ? 'tabItem active' : 'tabItem'}>
+          <Link
+            className="tabLink"
+            to={{ pathname: pathname, hash: `#${area}` }}
+          >
+            리뷰
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+}
+export default withRouter(ContentsTab);
