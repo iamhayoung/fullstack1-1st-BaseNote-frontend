1547bab474b6eaf6af1d51fe613e9c13cb569359
Hayoung Gam <hahhyoung@gmail.com>
2021-07-09 03:28:17 +0900

    Add: 목데이터 fetch, 상세정보 내부 컨텐츠에 따라 컴포넌트 분리

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -15,7 +15,7 @@ class Routes extends Component {
           <Route exact path="/category" component={Category} />
           <Route exact path="/member" component={Member} />
           <Route exact path="/order" component={Order} />
-          <Route exact path="/product" component={Product} />
+          <Route exact path="/product/:id" component={Product} />
         </Switch>
       </Router>
     );
--- src/pages/product/ProductDetailInfo/ProductDetailInfo.jsx
+++ src/pages/product/ProductDetailInfo/ProductDetailInfo.jsx
@@ -1,60 +1,57 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import ContentsTab from '../Components/ContentsTab';
-import Hashtags from './Hashtags';
-import NoteTable from './NoteTable';
-import { ReactComponent as Logo } from '../../../assets/logo-BASENOTE_slogan.svg';
+import ProductIntro from './ProductIntro';
+import NoteInfo from './NoteInfo';
+import Keyword from './Keyword';
 import './ProductDetailInfo.scss';
 
 class ProductDetailInfo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      productData: {},
+    };
+  }
+
+  getProductData = async () => {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+
+    try {
+      const response = await fetch('http://localhost:3000/data/mockData.json');
+      if (!response.ok) {
+        throw new Error(`HTTP Status code: ${response.status}`);
+      }
+
+      const result = await response.json();
+      const productData = result.products.filter(
+        data => data.id === parseInt(id)
+      );
+      return this.setState({ productData: productData[0] });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getProductData();
+  }
+
   render() {
+    const { productData } = this.state;
     return (
       <section className="ProductDetailInfo" id="detail">
         <ContentsTab area="detail" />
-        <div className="intro">
-          <Logo />
-          <p className="sentence">
-            향기에는 가장 섬세하고 다양한 취향이 담겨있습니다.
-            <br />
-            그렇지만 어디 내가 좋아하는 향수 찾기가 쉬웠나요.
-            <br />
-            좋은 원료로 만든 다양한 향수를, 더 쉽게 경험할 수 있도록.
-            <br />
-            가장 '나다운 향'을 찾기 위한 여정을 함께 합니다.
-          </p>
-        </div>
-        <div className="noteInfo">
-          <h3 className="title">note</h3>
-          <p className="noteDesc">
-            농도 짙은 자스민과 녹진한 바닐라, 짙게 깔린 우드 노트의 찐득한 만남
-          </p>
-          <Hashtags tag="달달한" />
-          <NoteTable />
-        </div>
-        <div className="keyword">
-          <h3 className="title">keyword</h3>
-          <p className="sentence">
-            향 전문가와 사용자들의 느낌을 키워드로 표현하였습니다.
-            <br />
-            사용자와 주변 사람들이 어떻게 느낄지 유추할 수 있습니다.
-          </p>
-          <Hashtags tag="melt01" />
-          <div className="bubbles">
-            <div className="circle xlarge">달달한</div>
-            <div className="circle xlarge">어른스러운</div>
-            <div className="circle xlarge">바닐라</div>
-            <div className="circle large">따뜻한</div>
-            <div className="circle large">무거운</div>
-            <div className="circle large">디저트</div>
-            <div className="circle medium">복합적인</div>
-            <div className="circle medium">부드러운</div>
-            <div className="circle medium">과일</div>
-            <div className="circle small">차분한</div>
-            <div className="circle small">지적인</div>
-            <div className="circle small">인센스</div>
-          </div>
-        </div>
+        <ProductIntro {...productData} />
+        <NoteInfo {...productData} />
+        <Keyword {...productData} />
       </section>
     );
   }
 }
-export default ProductDetailInfo;
+
+export default withRouter(ProductDetailInfo);
