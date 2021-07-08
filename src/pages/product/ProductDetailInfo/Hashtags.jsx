c5c44e84539d25a89b4b56005df2d1bd3338805e
Hayoung Gam <hahhyoung@gmail.com>
2021-07-09 03:56:38 +0900

    Modify: 상세페이지 상세정보 Hashtags 컴포넌트 수정 (목데이터 이용)

--- src/pages/product/ProductDetailInfo/Hashtags.jsx
+++ src/pages/product/ProductDetailInfo/Hashtags.jsx
@@ -3,10 +3,17 @@ import './Hashtags.scss';
 
 class Hashtags extends Component {
   render() {
-    const { tag } = this.props;
+    const { first_mood, series, seriesNumber, name } = this.props;
     return (
       <div className="hashtags">
-        <p className="tag">{`#${tag}`}</p>
+        {first_mood &&
+          first_mood.map(mood => (
+            <p key={mood} className="tag">{`#${mood}`}</p>
+          ))}
+        {series && seriesNumber && (
+          <p className="tag">{`#${series}${seriesNumber}`}</p>
+        )}
+        {name && <p className="tag">{`#${name.replace(/\s+/g, '')}`}</p>}
       </div>
     );
   }
