f13a4ba5e7306438eb1ad1ef0bac3feb0cb200e9
Minseongkimm <84434605+Minseongkimm@users.noreply.github.com>
2021-07-09 10:15:28 +0900

    Add:Shop 부분 hidden menu구현

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -3,41 +3,74 @@ import './TopNav.scss';
 import { BsPerson } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import { ReactComponent as MainLogo } from '../../assets/logo-BASENOTE_icon.svg';
-// import member from '../../pages/member/Member';
 
 class TopNav extends Component {
   render() {
     return (
-      <div className="TopNav">
-        <ul className="navCategory">
-          <a
-            href="https://paffem.me/perfume_teller.html"
-            className="navCategoryLink"
-          >
-            Perfume Teller
-          </a>
-          <Link to="/" className="navCategoryLink">
-            SHOP
+      <div className="TopNavWrapper">
+        <div className="TopNav shopHover">
+          <ul className="navCategory">
+            <a
+              href="https://paffem.me/perfume_teller.html"
+              className="navCategoryLink"
+            >
+              Perfume Teller
+            </a>
+            <Link to="/" className="navCategoryLink hiddenButton">
+              SHOP
+              <ul className="hideMenu">
+                <Link to="/" className="hiddenMenuTitle">
+                  2.5ml / 데일리키트
+                </Link>
+                <Link to="/" className="hiddenMenuTitle">
+                  40ml
+                </Link>
+                <Link to="/" className="hiddenMenuTitle">
+                  SET
+                </Link>
+                <Link to="/" className="hiddenMenuTitle">
+                  GOODS
+                </Link>
+              </ul>
+            </Link>
+            <Link to="/" className="navCategoryLink">
+              ABOUT
+            </Link>
+            <Link to="/" className="navCategoryLink">
+              EVENT
+            </Link>
+          </ul>
+          <Link to="메인컴포넌트" className="NavTitle">
+            <MainLogo />
           </Link>
-          <Link to="/" className="navCategoryLink">
-            ABOUT
-          </Link>
-          <Link to="/" className="navCategoryLink">
-            EVENT
-          </Link>
-        </ul>
-        <Link to="메인컴포넌트" className="NavTitle">
-          <MainLogo />
-        </Link>
-        <div className="NavIcons">
-          <Link to="/member">
-            <BsPerson className="LoginIcon" />
-          </Link>
+          <div className="NavIcons">
+            <Link to="/member">
+              <BsPerson className="LoginIcon" />
+            </Link>
+          </div>
         </div>
+        {/* <div className="hideMenu"> */}
+        {/* <ul className="hideMenu">
+          <Link to="/" className="hiddenMenuTitle">
+            2.5ml / 데일리키트
+          </Link>
+          <Link to="/" className="hiddenMenuTitle">
+            40ml
+          </Link>
+          <Link to="/" className="hiddenMenuTitle">
+            SET
+          </Link>
+          <Link to="/" className="hiddenMenuTitle">
+            GOODS
+          </Link>
+        </ul> */}
+        {/* </div> */}
+        {/* <div className="nono"> dkfjdlkfjklsdjf</div> */}
       </div>
     );
   }
 }
+
 export default TopNav;
 
 //나중에 컴포넌트 완성시 링크 수정할것//
