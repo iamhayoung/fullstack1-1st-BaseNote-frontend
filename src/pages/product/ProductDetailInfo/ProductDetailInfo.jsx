e113d01b0fc2a47dc227a86df3d9b9fe8f6084ed
Hayoung Gam <hahhyoung@gmail.com>
2021-07-11 17:24:33 +0900

    Modify: 상품 상세정보 Fetch 구문을 Product.jsx로 이동 (DetailHead에서도 공통으로 사용하기 때문)

--- src/pages/product/ProductDetailInfo/ProductDetailInfo.jsx
+++ src/pages/product/ProductDetailInfo/ProductDetailInfo.jsx
@@ -8,38 +8,8 @@ import ConceptImage from './ConceptImage';
 import './ProductDetailInfo.scss';
 
 class ProductDetailInfo extends Component {
-  constructor() {
-    super();
-    this.state = {
-      productData: {},
-    };
-  }
-
-  getProductData = async () => {
-    const { id } = this.props.match.params;
-
-    try {
-      const response = await fetch('http://localhost:3000/data/mockData.json');
-      if (!response.ok) {
-        throw new Error(`HTTP Status code: ${response.status}`);
-      }
-
-      const result = await response.json();
-      const productData = result.products.filter(
-        data => data.id === parseInt(id)
-      );
-      return this.setState({ productData: productData[0] });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  componentDidMount() {
-    this.getProductData();
-  }
-
   render() {
-    const { productData } = this.state;
+    const { productData } = this.props;
     const { keywords } = productData;
 
     return (
