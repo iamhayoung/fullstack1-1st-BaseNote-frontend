2ee07abf55b513b4050f25370a615e1292932cac
Hayoung Gam <hahhyoung@gmail.com>
2021-07-13 04:03:03 +0900

    Add: 장바구니 페이지 라우팅

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -8,7 +8,7 @@ import {
 import Category from './pages/category/Category';
 import Main from './pages/main/Main';
 import Member from './pages/member/Member';
-import Order from './pages/order/Order';
+import * as Order from './pages/order';
 import Product from './pages/product/Product';
 
 class Routes extends Component {
@@ -19,7 +19,7 @@ class Routes extends Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/category/:volume" component={Category} />
           <Route exact path="/member" component={Member} />
-          <Route exact path="/order" component={Order} />
+          <Route exact path="/order/cart" component={Order.Cart} />
           <Route exact path="/product/:volume/:id" component={Product} />
           <Redirect path="*" to="/" />
         </Switch>
--- src/pages/order/Order.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-import './Order.scss';
-
-class Order extends Component {
-  render() {
-    return <p> 오더 페이지</p>;
-  }
-}
-export default Order;
