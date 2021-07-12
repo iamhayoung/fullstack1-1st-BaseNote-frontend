bc2a6a178d45c2a40c6259fc9808ac023a6b4cca
Minseongkimm <84434605+Minseongkimm@users.noreply.github.com>
2021-07-12 11:27:35 +0900

    Modify:용량에따른 페이지 나눔으로 인해 Routes 수정

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Category from './pages/category/Category';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import Main from './pages/main/Main';
-import Member from './pages/member/Member';
+import Category from './pages/category/Category';
+import Login from './pages/member/Login';
+import Signup from './pages/member/Signup';
 import Order from './pages/order/Order';
 import Product from './pages/product/Product';
-
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/category" component={Category} />
-          <Route exact path="/member" component={Member} />
+          <Route exact path="/category/2.5ml" component={Category} />
+          <Route exact path="/category/40ml" component={Category} />
+          <Route exact path="/member/login" component={Login} />
+          <Route exact path="/member/signup" component={Signup} />
           <Route exact path="/order" component={Order} />
-          <Route exact path="/product" component={Product} />
+          <Route exact path="/product/2.5ml/:id" component={Product} />
+          <Route exact path="/product/40ml/:id" component={Product} />
+          <Redirect path="*" to="/" />
         </Switch>
       </Router>
     );
   }
 }
-
 export default Routes;
