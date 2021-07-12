b21b55c4602e3d575d64b281bbbe6fd7d329564d
Hayoung Gam <hahhyoung@gmail.com>
2021-07-12 15:04:41 +0900

    Remove: ContentsTab 컴포넌트 삭제

--- src/pages/product/ProductDetailInfo/ProductDetailInfo.jsx
+++ src/pages/product/ProductDetailInfo/ProductDetailInfo.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ContentsTab from '../Components/ContentsTab';
 import ProductIntro from './ProductIntro';
 import NoteInfo from './NoteInfo';
 import Keyword from './Keyword';
@@ -7,20 +6,12 @@ import ConceptImage from './ConceptImage';
 import './ProductDetailInfo.scss';
 
 class ProductDetailInfo extends Component {
-  constructor() {
-    super();
-    this.detailInfoRef = React.createRef();
-  }
-
   render() {
     const { productData } = this.props;
     const { keywords } = productData;
-    console.log(productData);
-    console.log('this.detailInfoRef', this.detailInfoRef);
 
     return (
-      <section className="ProductDetailInfo" ref={this.detailInfoRef}>
-        <ContentsTab ref={this.detailInfoRef} area="detail" />
+      <section className="ProductDetailInfo">
         <ProductIntro {...productData} />
         <NoteInfo {...productData} />
         {keywords && <Keyword {...productData} />}
--- src/pages/product/ProductReview/ProductReview.jsx
+++ src/pages/product/ProductReview/ProductReview.jsx
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import ContentsTab from '../Components/ContentsTab';
 import './ProductReview.scss';
 
 class ProductReview extends Component {
-  constructor() {
-    super();
-    this.reviewRef = React.createRef();
-  }
-
   render() {
-    console.log('this.reviewRef', this.reviewRef);
     return (
-      <section className="ProductReview" ref={this.reviewRef}>
-        <ContentsTab ref={this.reviewRef} area="review" />
+      <section className="ProductReview">
         <p>ProductReview 영역</p>
       </section>
     );
