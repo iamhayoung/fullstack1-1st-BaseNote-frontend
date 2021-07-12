df45dfa8370915b22ca4f851cfafd7b07511861b
songmiO <dhthd1060@gmail.com>
2021-07-13 01:09:48 +0900

    Add : 로그인 기능 추가 및 리펙토링 완료

--- src/pages/member/Signup.jsx
+++ src/pages/member/Signup.jsx
@@ -6,7 +6,7 @@ class SignUp extends Component {
       <div>
         <h1>
           Signup 레이아웃은 다른 Signup 브랜치에 PR 보냈습니다. 확인
-          부탁드립니다!~~
+          부탁드립니다!~
         </h1>
       </div>
     );
