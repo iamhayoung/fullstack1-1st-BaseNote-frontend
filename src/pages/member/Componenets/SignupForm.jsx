8fe6d1ff352db1d799787d770c73af27ad8e9ba3
songmiO <dhthd1060@gmail.com>
2021-07-12 09:47:45 +0900

    Add : 회원가입 레이아웃완료

--- src/pages/member/Componenets/SignupForm.jsx
+++ src/pages/member/Componenets/SignupForm.jsx
@@ -52,6 +52,10 @@ class SignupForm extends Component {
             type="password"
             placeholder="비밀번호를 다시 입력해주세요"
           />
+          <input type="checkbox" name="agree" value="1"></input>
+          <label className="eventAgreement">
+            이벤트 및 할인 소식 알림 동의 (선택)
+          </label>
         </div>
         <button className="signUpBtn">동의하고 회원가입</button>
       </form>
