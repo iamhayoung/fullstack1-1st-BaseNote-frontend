cb18e9351acdc5b23e095a31c8fc35165163ae7c
Hayoung Gam <hahhyoung@gmail.com>
2021-07-13 13:14:43 +0900

    Add: import React 추가

--- src/pages/order/Cart/CartTable.jsx
+++ src/pages/order/Cart/CartTable.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import HeadRow from './HeadRow';
 import DataRow from './DataRow';
 import './CartTable.scss';
--- src/pages/order/Cart/DataRow.jsx
+++ src/pages/order/Cart/DataRow.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './DataRow.scss';
 
 class DataRow extends Component {
