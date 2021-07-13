76c0aab3f9f85aec72fde83414680e7e4a286db5
songmiO <dhthd1060@gmail.com>
2021-07-13 21:48:32 +0900

    Modify: 코드 수정

---
+++ src/pages/member/Member.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Member extends Component {
+  render() {
+    return (
+      <div>
+        <p>멤버 페이지</p>
+      </div>
+    );
+  }
+}
+
+export default Member;
