62958e9729181739fecf2e98e8949d2a5284e13b
Minseongkimm <84434605+Minseongkimm@users.noreply.github.com>
2021-07-14 22:24:25 +0900

    Add:2번째 이미지까지 완료

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -7,10 +7,16 @@ import './Main.scss';
 // import PeopleWalking from './Videos/PeopleWalking.mp4';
 // import Road from './Videos/Road.mp4';
 import SwimmingPool from './Videos/SwimmingPool.mp4';
+
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
-      <>
+      <div className="main">
         <TopNav />
         <video
           autoPlay
@@ -20,8 +26,24 @@ class Main extends Component {
           type="video/mp4"
           className="mainVideo"
         ></video>
-        <p className="mainVideoTitle">가나다라마바사</p>{' '}
-      </>
+        <div className="topImageTitleWrapper">
+          <div className="mainVideoTitle">"그 시절 여름의 향기"</div>
+          <div className="mainVideoTitle2">SUMMER EDITION'3</div>
+          <button className="findDetails">자세히보기</button>
+        </div>
+        <img
+          src="https://cdn.pixabay.com/photo/2015/04/20/13/38/furniture-731449_1280.jpg"
+          alt="들판이미지"
+          className="mainImage"
+        />
+        <div className="secondImageTitleWrapper">
+          <div className="mainPictureTitle">Change For Signiture</div>
+          <div className="mainPictureTitle2">
+            시그니처 향수를 찾기위한 교환 서비스
+          </div>
+          <button className="changeService"> 교환서비스</button>
+        </div>
+      </div>
     );
   }
 }
