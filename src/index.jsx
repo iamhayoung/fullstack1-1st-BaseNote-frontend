28260dbecb7aa1f9f6f94f3d01657cac1c5ef105
Minseongkimm <84434605+Minseongkimm@users.noreply.github.com>
2021-07-20 17:07:09 +0900

    Modify: 하영님 피드백 반영

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import './styles/common.scss';
-import './styles/_reset.scss';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
