8b62b7a5d6c838ba58cb2201ab782ccd191fccff
songmiO <dhthd1060@gmail.com>
2021-07-21 12:57:27 +0900

    Modify : Login.scss 파일 내 mixin 사용

--- src/pages/member/Login/Login.jsx
+++ src/pages/member/Login/Login.jsx
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import LoginTitle from './LoginTitle';
 import LoginForm from './LoginForm';
 import OtherLoginButton from './OtherLoginButton';
+import Container from '../../../components/Container/Container';
 import './Login.scss';
 
 class Login extends Component {
   render() {
     return (
-      <div className="Login">
+      <div className="login">
         <div>
           <LoginTitle />
           <LoginForm />
           <OtherLoginButton />
+          <Container />
         </div>
       </div>
     );
